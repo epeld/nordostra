37703b1c0d3a66e3d4c7440499325b57fa6fc015
Erik Peldan <epe@trifork.com>
2015-02-28 22:12:03 +0100

    make gulp write to django

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,26 @@ var gulp = require('gulp');
 var jade = require('gulp-jade');
 var stylus = require('gulp-stylus');
 
+var outdir = 'django/nordostra/static/';
+
 gulp.task('html', function() {
     return gulp.src('jade/*.jade')
                 .pipe(jade())
-                .pipe(gulp.dest('build/html'));
+                .pipe(gulp.dest(outdir + 'html'));
 });
 
 gulp.task('styles', function() {
     return gulp.src('stylus/*.styl')
         .pipe(stylus())
-        .pipe(gulp.dest('build/styles'));
+        .pipe(gulp.dest(outdir + 'styles'));
 });
 
 gulp.task('images', function() {
-    return gulp.src('images/*').pipe(gulp.dest("build/images"));
+    return gulp.src('images/*').pipe(gulp.dest(outdir + "images"));
 });
 
 gulp.task('fonts', function() {
-    return gulp.src('fonts/*').pipe(gulp.dest("build/fonts"));
+    return gulp.src('fonts/*').pipe(gulp.dest(outdir + "fonts"));
 });
 
 gulp.task('default', ['styles', 'html', 'images', 'fonts']);
