99b7fdd881b29366774a17ccd985e3a600d7b82b
Erik Peldan <epe@trifork.com>
2015-03-01 16:54:22 +0100

    displat coming and past sessions and allow editing them

--- django/nordostra/attendance/models.py
+++ django/nordostra/attendance/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Session(models.Model):
+from django.contrib.contenttypes.models import ContentType
+from django.core.urlresolvers import reverse
+
+
+class AdminURLMixin(object):
+    def get_admin_url(self):
+        content_type = ContentType.objects.get_for_model(self.__class__)
+        return reverse("admin:%s_%s_change" % ( content_type.app_label, content_type.model), args=(self.id,))
+
+class Session(models.Model, AdminURLMixin):
     """Represents e.g a training session for which we want to register attendees"""
 
     date = models.DateField()
--- django/nordostra/attendance/views.py
+++ django/nordostra/attendance/views.py
@@ -11,8 +11,6 @@ from attendance.models import Session
 def past_sessions(request):
     num_users = User.objects.count()
     sessions = Session.objects.filter(date__lte=datetime.date.today()).order_by("-date")
-    env = { 'sessions': sessions, 'user': request.user, 'num_users': num_users }
-    if request.user.is_staff:
-        return render_to_response('jade/sessions.jade', env)
-    else:
-        return render_to_response('jade/nav.jade', env)
+    coming = Session.objects.filter(date__gt=datetime.date.today()).order_by("-date")
+    env = { 'sessions': sessions, 'user': request.user, 'num_users': num_users, 'coming': coming }
+    return render_to_response('jade/sessions.jade', env)
